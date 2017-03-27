ca35888e848e51e7d95dcdd07bda636df4247b17
Srinath Alla <srinath.alla@kony.com>
2017-03-27 15:25:38 +0530

    Linked List with Integer Operations like addlists,addListsWithDigitsStoredInReverseOrder

---
+++ algo/src/main/java/com/sri/algo/LinkedList.java
@@ -0,0 +1,66 @@
+package com.sri.algo;
+
+public class LinkedList<T> {
+
+	private Node root;
+	private Node tail;
+	
+	public Node getRoot() {
+		return root;
+	}
+
+	public LinkedList<T> addEntry(T data) {
+		Node entry = new Node().withData(data);
+		root = root == null ? entry : entry.withNext(root);
+		return this;
+	}
+
+	public LinkedList<T> addEntryToTail(T data) {
+		Node entry = new Node().withData(data);
+		root = root == null ? entry : root;
+		tail = tail == null ? root : tail.withNext(entry).next;
+		return this;
+	}
+
+	public LinkedList<T> withDataSet(T... elements) {
+		for (T element : elements) {
+			addEntryToTail(element);
+		}
+		return this;
+	}
+
+	public String toString() {
+		StringBuilder elements = new StringBuilder();
+
+		Node entry = root;
+		while (entry != null) {
+			elements.append(entry.data);
+			elements.append("-->");
+			entry = entry.next;
+		}
+		
+		elements.replace(elements.lastIndexOf("-->"), elements.length(), "");
+
+		return elements.toString();
+	}
+
+	class Node {
+		T data;
+		Node next;
+
+		Node withData(T data) {
+			this.data = data;
+			return this;
+		}
+
+		Node withNext(Node next) {
+			this.next = next;
+			return this;
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println(new LinkedList<Integer>().withDataSet(1, 2, 3, 4, 5).toString());
+	}
+
+}
