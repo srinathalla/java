75039b75aa885986965f66dcc33feb7f94c9085c
Srinath Alla <srinath.alla@kony.com>
2017-04-05 20:10:57 +0530

    Queue implementation using linked list.

---
+++ algo/src/main/java/com/sri/algo/MyQueue.java
@@ -0,0 +1,51 @@
+package com.sri.algo;
+
+//Queue implementation using linked list.
+public class MyQueue<T> {
+
+	private LinkedList<T> delegatedLinkedList = new LinkedList<>();
+
+	public void add(T data) {
+		delegatedLinkedList.addEntryToTail(data);
+	}
+
+	public T remove() {
+		T data = delegatedLinkedList.getRoot() != null ? delegatedLinkedList.getRoot().data : null;
+		delegatedLinkedList.removeHead();
+		return data;
+	}
+
+	public T peek() {
+		return delegatedLinkedList.getRoot() != null ? delegatedLinkedList.getRoot().data : null;
+	}
+
+	public int size() {
+		return delegatedLinkedList.length();
+	}
+
+	public boolean isEmpty() {
+		return size() == 0;
+	}
+
+	@SuppressWarnings("unchecked")
+	public void addAll(T... elements) {
+		for (T t : elements) {
+			add(t);
+		}
+	}
+
+	public String toString() {
+		return delegatedLinkedList.toString();
+	}
+
+	public static void main(String[] args) {
+		MyQueue<Integer> queue = new MyQueue<>();
+		queue.addAll(1, 2, 3, 4, 5);
+		System.out.println(queue.toString());
+
+		queue.remove();
+		queue.remove();
+		System.out.println(queue.toString());
+	}
+
+}
